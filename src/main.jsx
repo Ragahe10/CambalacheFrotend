8d2319c5a64951392db7ce4dd40ec7c5383ee204
Ragahe10 <ragahe10@gmail.com>
2024-04-27 17:28:00 -0300

    base del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
+import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TypesExample from './prueba.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
